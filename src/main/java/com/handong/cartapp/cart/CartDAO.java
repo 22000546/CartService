5e4e6f61464a1f80606fdace013ed42f76808614
Yeeun <22000546@handong.edu>
2021-12-12 01:56:43 +0900

    Add CartDAO.java

---
+++ src/main/java/com/handong/cartapp/cart/CartDAO.java
@@ -0,0 +1,28 @@
+package com.handong.cartapp.cart;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CartDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public int insertCart(CartVO vo) {
+		int result = sqlSession.insert("Cart.insertCart", vo);
+		return result;
+	}
+	
+	public int updateCart(CartVO vo) {
+		int result = sqlSession.update("Cart.updateCart", vo);
+		return result;
+	}
+	
+	public int deleteCart(int seq) {
+		int result = sqlSession.delete("Cart.deleteCart", seq);
+		return result;
+	}
+
+}
