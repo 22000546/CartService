dbc14a0ef0f55d4c511eac2ee388f58f4cabdc76
Yeeun <22000546@handong.edu>
2021-12-12 17:41:05 +0900

    Update CartController to control editform

--- src/main/java/com/handong/cartapp/cart/CartController.java
+++ src/main/java/com/handong/cartapp/cart/CartController.java
@@ -2,6 +2,8 @@ package com.handong.cartapp.cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +25,24 @@ public class CartController {
 			System.out.println("데이터 추가 실패");
 		} else {
 			System.out.println("데이터 추가 성공");
+		}
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/cart/edit/{id}", method = RequestMethod.GET)
+	public String editItem(@PathVariable("id") int id, Model model) {
+		CartVO cartVO = cartService.getCart(id);
+		model.addAttribute("cartVO", cartVO);
+		return "cart/editform";
+	}
+	
+	@RequestMapping(value = "/cart/editok", method = RequestMethod.POST)
+	public String editItemOK(CartVO vo) {
+		int i = cartService.updateCart(vo);
+		if(i == 0) {
+			System.out.println("데이터 수정 실패");
+		} else {
+			System.out.println("데이터 수정 성공");
 		}
 		return "redirect:list";
 	}
