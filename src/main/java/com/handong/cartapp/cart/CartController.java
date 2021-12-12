42ef5e0d39aa5fb9f74761af5e015210853f31f0
Yeeun <22000546@handong.edu>
2021-12-12 17:33:45 +0900

    Add CartController (Implement add)

---
+++ src/main/java/com/handong/cartapp/cart/CartController.java
@@ -0,0 +1,30 @@
+package com.handong.cartapp.cart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CartController {
+	
+	@Autowired
+	CartService cartService;
+	
+	@RequestMapping(value = "/cart/add", method = RequestMethod.GET)
+	public String addItem() {
+		return "cart/addform";
+	}
+	
+	@RequestMapping(value = "/cart/addok", method = RequestMethod.POST)
+	public String addItemOK(CartVO vo) {
+		int i = cartService.insertCart(vo);
+		if(i == 0) {
+			System.out.println("데이터 추가 실패");
+		} else {
+			System.out.println("데이터 추가 성공");
+		}
+		return "redirect:list";
+	}
+
+}
