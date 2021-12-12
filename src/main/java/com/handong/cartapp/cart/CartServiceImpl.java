dd9d0a2dcf19f1ca8dd07ac11a60dcc9ea97d6dc
Yeeun <22000546@handong.edu>
2021-12-12 10:43:35 +0900

    Fix error (add annotation)

--- src/main/java/com/handong/cartapp/cart/CartServiceImpl.java
+++ src/main/java/com/handong/cartapp/cart/CartServiceImpl.java
@@ -1,7 +1,9 @@
 package com.handong.cartapp.cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartServiceImpl implements CartService {
 	
 	@Autowired
