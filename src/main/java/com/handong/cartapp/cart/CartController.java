73d327b048d1d929e8261123d7882015ddd12731
Yeeun <22000546@handong.edu>
2021-12-18 19:47:05 +0900

    Update CartController.java for file upload

--- src/main/java/com/handong/cartapp/cart/CartController.java
+++ src/main/java/com/handong/cartapp/cart/CartController.java
@@ -1,11 +1,17 @@
 package com.handong.cartapp.cart;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class CartController {
@@ -19,7 +25,18 @@ public class CartController {
 	}
 	
 	@RequestMapping(value = "/cart/addok", method = RequestMethod.POST)
-	public String addItemOK(CartVO vo) {
+	public String addItemOK(CartVO vo) throws IllegalStateException, IOException {
+		String fileName = null;
+		MultipartFile uploadFile = vo.getUploadFile();
+		if (!uploadFile.isEmpty()) {
+			String originalFileName = uploadFile.getOriginalFilename();
+			String ext = FilenameUtils.getExtension(originalFileName);	//확장자 구하기
+			UUID uuid = UUID.randomUUID();	//UUID 구하기
+			fileName = uuid+"."+ext;
+			uploadFile.transferTo(new File("D:\\upload\\" + fileName));
+		}
+		vo.setImage(fileName);
+		System.out.println("fileName : " + fileName);
 		int i = cartService.insertCart(vo);
 		if(i == 0) {
 			System.out.println("데이터 추가 실패");
